a858d4e11a68c2ccaf4d7daa3486f239573a2c1e
nikitamurashka <nikita.murashka@gmail.com>
2024-09-22 21:25:42 +0300

    feat: KanbanView

---
+++ src/app/i/tasks/SwitcherView.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import clsx from 'clsx'
+import { Kanban, ListTodo } from 'lucide-react'
+
+import type { TypeView } from './TasksView.tsx'
+
+interface ISwitcherView {
+  type: TypeView
+  setType: (value: TypeView) => void
+}
+
+export function SwitcherView({ type, setType }: ISwitcherView) {
+  return (
+    <div className='flex items-center gap-4 mb-5'>
+      <button
+        className={clsx(
+          'flex items-center gap-1',
+          type === 'kanban' && 'opacity-40'
+        )}
+        onClick={() => setType('list')}
+      >
+        <ListTodo />
+        List
+      </button>
+      <button
+        className={clsx(
+          'flex items-center gap-1',
+          type === 'list' && 'opacity-40'
+        )}
+        onClick={() => setType('kanban')}
+      >
+        <Kanban />
+        Kanban
+      </button>
+    </div>
+  )
+}
--- src/app/i/tasks/TasksView.tsx
+++ src/app/i/tasks/TasksView.tsx
@@ -1,7 +1,31 @@
+'use client'
+
+import Loader from '@/components/ui/Loader'
+
+import { SwitcherView } from './SwitcherView'
+import { useLocalStorage } from './hooks/useLocalStorage'
+import { KanbanView } from './kanban-view/KanbanView'
 import { ListView } from './list-view/ListView'
 
+export type TypeView = 'list' | 'kanban'
+
 export function TasksView() {
-  // TODO: Add second view
-  // TODO: Add Suspense
-  return <ListView />
+  const [type, setType, isLoading] = useLocalStorage<TypeView>({
+    key: 'view-type',
+    defaultValue: 'list'
+  })
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  return (
+    <div>
+      <SwitcherView
+        type={type}
+        setType={setType}
+      />
+      {type === 'list' ? <ListView /> : <KanbanView />}
+    </div>
+  )
 }
